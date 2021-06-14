37e71b54b04aefba432edddb1eabe449ba1d9f5b
Petikov Vladimir <vx7xv@inbox.ru>
2021-06-14 12:28:50 +0300

    Коммит-01 Учебный проект начат 13.01.21

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <LinkList></LinkList>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import LinkList from './components/LinkList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LinkList
+  }
+}
+</script>
+
+<style lang="scss">
+    .wrapper {
+      width: 80%;
+      margin: auto;
+    }
+</style>
+
+
---
+++ src/components/LinkList.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="root">
+        <ul class="list">
+          <li class="list-item" v-for="(title, index) in titles" :key="index" @click="getItem(title)"> {{getTitle(title)}} </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'LinkList',
+  data() {
+    return {
+       titles: [{title: 'Первый'},
+                {title: 'Второй'},
+                {title: 'Третий'},
+                {title: 'Четвертый'},
+                {title: 'Десятый'},
+                {title: 'Шестой'}]
+    }
+  },
+  methods: {
+      getTitle(title) {
+        return `${title.title}`;
+      },
+      getItem(title) {
+         console.log(title.title);
+      }
+  }
+}
+</script>
+<style lang="scss">
+  @import "../assets/scss/variables";
+  .list {
+    display: flex;
+    font-family: sans-serif;
+    font-size: 16px;
+    font-weight: 600;
+    color: $main-color-list-item;
+    list-style: none;
+    outline: none;
+    &-item {
+      &:not(:last-child) {
+        margin-right: 30px;
+      }
+      &:hover {
+        color: $hover-color-list-item;
+      }
+    }
+  }
+</style>
